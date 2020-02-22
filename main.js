6ddb9d533c0115f9d244cf0a84fbff0e380c51cd
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-02-22 10:59:13 -0600

    can view all dinos

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 const dinos = []
 
+const printToDom = (divId, textToPrint) => {
+  const selectedDiv = document.getElementById(divId);
+  selectedDiv.innerHTML = textToPrint;
+}
+
+const printDinos = (dinoArray) => {
+  let domString = '';
+  for (let i = 0; i < dinoArray.length; i++) {
+    domString += '<div class="col-4">'
+    domString +=  '<div class="card">'
+    domString +=     `<img src="${dinoArray[i].imageUrl}" class="card-img-top" alt="dino image">`
+    domString +=    '<div class="card-body">'
+    domString +=       `<h5 class="card-title">${dinoArray[i].name}</h5>`
+    domString +=      `<p class="card-text">Health: ${dinoArray[i].health}</p>`
+    domString +=    '</div>'
+    domString +=  '</div>'
+    domString += '</div>'
+  }
+  printToDom('kennel', domString);
+}
+
+// https://images-na.ssl-images-amazon.com/images/I/61fC04pumjL._AC_SL1001_.jpg
+
 const newDino = (e) => {
   e.preventDefault();  //prevents page refresh when Submit is clicked, this is default on Forms
   const brandNewDino = {
@@ -15,8 +38,7 @@ const newDino = (e) => {
   dinos.push(brandNewDino);
   document.getElementById('new-dino-form').reset();
   document.getElementById('collapseOne').classList.remove('show');
-
-  console.log(dinos);
+  printDinos(dinos);
 };
 
 const init = () => {
